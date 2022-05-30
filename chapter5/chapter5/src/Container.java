9ebe17cf6e832edcfa49701b73d82a4b8c6c8485
a-ntw <alvintwng@gmail.com>
2022-05-30 23:46:46 +0800

    Proj0502

---
+++ chapter5/chapter5/src/Container.java
@@ -0,0 +1,33 @@
+
+/**
+ * 3. Create a class to represent a container. The class Container should have 
+ * the following properties.
+ *  a. Maximum capacity of the container in liters.
+ *  b. Quantity of liquid at any given time in liters.
+ * The following operations can be performed on the containers:
+ *  a. Completely fill a container.
+ *  b. Completely empty a container.
+ *  c. Transfer liquid from one container to another.
+ * Define the class named Container that implements the properties and 
+ * operations defined above. Create a constructor of the Container class that 
+ * allows the user to specify the maximum capacity of the container in liters. 
+ * Initially, assume that all the containers are empty.
+ * Also, implement the following methods in the Container class.
+ *  a. Quantity to return the current quantity of liquid at any given time in 
+ *  liters.
+ *  b. Leftover to return the quantity of liquid that can be filled in the 
+ *  current container before it is full.
+ *  c. Full to fill the current container fully.
+ *  d. Empty to make the container completely empty.
+ *  e. Transfer to transfer a certain amount of liquid from one container to 
+ *  another.
+ *  f. displayQuantity to display the current quantity in 
+ *  liters/ milliliters/ kiloliters.
+ * Note: While transferring liquid from one container to another, check the 
+ * maximum capacity of the container.
+ *
+ */
+
+public class Container {
+
+}
---
+++ chapter5/readme.md
@@ -0,0 +1,18 @@
+chapter5
+---
+[top]: topOfThePage
+
+ref | fileName | interest
+--- | --- | ---:
+ | [  ](   )  | ``
+Proj0404 | [ journal ]( chapter4/src/Journal.java )             | `j = new Journal( ... ); j.setTitle(j.getSubmissionDetails(j.getTitle())); j.displayDetails();`
+Proj0501 | [ BookStore.java ]( chapter5/src/BookStore.java )    | `private static int noOfBooksSold = 0;``public static void setNoOfBooksSold(int currSold) {`
+ | [  ](   )  | 
+ | [  ](   )  | 
+ | [  ](   )  | 
+ | [  ](   )  | 
+ | [  ](   )  | 
+
+[:top: Top](#top)
+
+---
