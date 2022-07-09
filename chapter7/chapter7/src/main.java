145bf56764e404cc6bdcb3c5f72b7b60ea4c3597
a-ntw <alvintwng@gmail.com>
2022-07-10 00:44:16 +0800

    init

---
+++ chapter7/chapter7/src/main.java
@@ -0,0 +1,25 @@
+
+/* 220709 Absolute Java  Chapter7 Programming Projects. */
+/**
+ * main.
+ *
+ * @author antw
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Absolute Java  Chapter7 Programming Projects.");
+
+    }
+}
+
+/* Sample dialogue
+antw@Mac-mini chapter7 % javac -d build/classes src/*.java
+antw@Mac-mini chapter7 % cd build/classes                 
+antw@Mac-mini classes % java main                        
+Absolute Java  Chapter7 Programming Projects.
+antw@Mac-mini classes % cd ../..                         
+antw@Mac-mini chapter7 % pwd
+/Users/antw/absoluteJ/chapter7/chapter7
+antw@Mac-mini chapter7 %
+*/
